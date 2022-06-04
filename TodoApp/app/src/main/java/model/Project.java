4bb326053c86ddf3b844daf60f487ad1ab4f9eaf
TheeCronos <vinicius.o.s@outlook.com>
2022-06-04 14:54:24 -0300

    Add Project

---
+++ TodoApp/app/src/main/java/model/Project.java
@@ -0,0 +1,65 @@
+package model;
+
+import java.util.Calendar;
+
+public class Project {
+    
+    private int id;
+    private String name;
+    private String description;
+    private Calendar createdAt;
+    private Calendar updatedAt;
+
+    public Project(int id, String name, String description, Calendar createdAt, Calendar updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Calendar getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Calendar createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Calendar getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Calendar updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" + "id=" + id + ", name=" + name + ", description=" + description + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + '}';
+    }
+}
